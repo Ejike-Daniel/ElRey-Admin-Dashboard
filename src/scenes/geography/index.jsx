125faf0f9e2872c38500107d7cc90b428bdee15d
Elrey009 <daniloejike@gmail.com>
2025-03-06 00:03:27 +0100

    modified geo chart

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Box, colors } from "@mui/material";
+import Header from "../../components/Header";
+import GeoChart from "../../components/GeoChart";
 
-const index = () => {
-  return <div></div>;
+const Line = () => {
+  return (
+    <Box m={"20px"}>
+      <Header title={"GEOGRAPHY CHART"} subtitle={"Simple Geography  Chart "} />
+      <Box
+        height={"75vh"}
+        border={` 1px solid ${colors.grey[100]}`}
+        borderRadius={"4px"}
+      >
+        <GeoChart />
+      </Box>
+    </Box>
+  );
 };
 
-export default index;
+export default Line;

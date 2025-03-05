4790906cd0063f818212f431fa7a3c488792f1f2
Elrey009 <daniloejike@gmail.com>
2025-03-05 02:43:39 +0100

    modified bar and pie charts

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import { ColorModeContext, useMode } from "./theme";
 import TopBar from "./scenes/global/TopBar";
 import Invocies from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
 import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 import Team from "./scenes/team";
 import { Route, Routes } from "react-router-dom";
 import SideBar from "./scenes/global/SideBar";
 import Dashboard from "./scenes/dashboard";
-import BarChart from "./components/BarChart";
-import PieChart from "./components/PieChart";
-import LineChart from "./components/LineChart";
-import GeoChart from "./components/GeoChart";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -28,12 +28,12 @@ function App() {
             <TopBar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/line" element={<LineChart />} />
+              <Route path="/line" element={<Line />} />
               <Route path="/team" element={<Team />} />
-              <Route path="/pie" element={<PieChart />} />
-              <Route path="/bar" element={<BarChart />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/bar" element={<Bar />} />
               <Route path="/invoices" element={<Invocies />} />
-              <Route path="/geography" element={<GeoChart />} />
+              <Route path="/geography" element={<Geography />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/form" element={<Form />} />
               <Route path="/faq" element={<FAQ />} />
